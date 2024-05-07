6aecf5918dee3fc595a752c36136dcb0be5e5bdf
asoa <abailey82@gmail.com>
2024-05-07 03:02:35 +0000

    add generalization scripts

--- scripts/4-generalize.sh
+++ scripts/4-generalize.sh
@@ -6,3 +6,4 @@ sudo truncate -s 0 /etc/machine-id
 sudo rm /var/lib/dbus/machine-id
 sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
 sudo rm -f /etc/netplan/*.yaml
+sudo truncate -s 0 /etc/hostname
